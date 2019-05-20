522ad30b5d423dbe6b2a23d787c9ed2d729bdf91
Abusch419 <abusch419@gmail.com>
2019-05-20 10:06:11 -0400

    updates DOM manipulation

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,13 +1,14 @@
 let friends = require("../data/friends.json")
 
-module.exports = function(app) {
-    app.post("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.post("/api/friends", function (req, res) {
         let newfriend = req.body;
         friends.push(newfriend);
-        res.json({ waiting: false, message: "You have a reservation"})
-    
+        let bestMatch = friends[0]
+        res.json(bestMatch)
+
     });
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     })
 
